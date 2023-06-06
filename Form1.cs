ecef88fdca2bbcfc6bf11b7eab16838fb983655e
Ariston <aristoncandido.nw@gmail.com>
2023-06-06 13:21:35 -0300

    Win Form com C#

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace FormsCSHARP;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        buttonCalcularMedia_Click();
+
+
+
+        
+    }
+}
